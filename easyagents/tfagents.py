506b53127a4b82179b5a35ffc17921ed14b56d38
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-08-13 10:12:35 +0200

    new implementation for reinforce agent (#14)

--- easyagents/tfagents.py
+++ easyagents/tfagents.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tf_agents.agents.dqn import dqn_agent
 from tf_agents.agents.ppo import ppo_agent
+from tf_agents.agents.reinforce import reinforce_agent
 from tf_agents.drivers.dynamic_episode_driver import DynamicEpisodeDriver
 from tf_agents.environments import py_environment
 from tf_agents.environments import suite_gym
@@ -356,3 +357,120 @@ class DqnAgent(TfAgent):
                 tf_lossInfo = tf_agent.train(experience=trajectories)
             self._train_iteration_completed(iteration, tf_lossInfo.loss)
         return
+
+class ReinforceAgent(TfAgent):
+    """ creates a new agent based on the Reinforce algorithm using the tfagents implementation.
+        Reinforce is also known as Vanilla Policy Gradient as it is the most basic policy gradient approach.
+
+        PPO is an actor-critic algorithm using 2 neural networks. The actor network
+        to predict the next action to be taken and the critic network to estimate
+        the value of the game state we are currently in (the expected, discounted
+        sum of future rewards when following the current actor network).
+
+        Args:
+        gym_env_name    :   name of an OpenAI gym environment to be used for training and evaluation
+        fc_layers       :   defines the neural network to be used, a sequence of fully connected
+                            layers of the given size. Eg (75,40) yields a neural network consisting
+                            out of 2 hidden layers, the first one containing 75 and the second layer
+                            containing 40 neurons.
+        training                : instance of config.Training to configure the #episodes used for training.
+        num_training_steps_in_replay_buffer : defines the size of the replay buffer in terms of game steps.
+        learning_rate           : value in (0,1]. Factor by which the impact on the policy update is reduced
+                                  for each training step. The same learning rate is used for the value and
+                                  the policy network.
+        reward_discount_gamma   : value in (0,1]. Factor by which a future reward is discounted for each step.
+        logging                 : instance of config.Logging to define the logging behaviour
+        num_training_steps_in_replay_buffer : size of the replay buffer
+
+        see also: https://spinningup.openai.com/en/latest/algorithms/vpg.html
+    """
+
+    def __init__(self,
+                 gym_env_name: str,
+                 fc_layers=None,
+                 training: Training = None,
+                 num_training_steps_in_replay_buffer: int = 10001,
+                 learning_rate: float = 0.001,
+                 reward_discount_gamma: float = 1,
+                 logging: Logging = None):
+        super().__init__(gym_env_name=gym_env_name,
+                         fc_layers=fc_layers,
+                         training=training,
+                         learning_rate=learning_rate,
+                         reward_discount_gamma=reward_discount_gamma,
+                         logging=logging)
+        assert num_training_steps_in_replay_buffer >= 1, "num_training_steps_in_replay_buffer must be >= 1"
+        self._num_training_steps_in_replay_buffer = num_training_steps_in_replay_buffer
+        return
+
+    def _train(self):
+        """ trains a policy using the gym_env.
+            Sets training_losses and training_average_returns, depending on the training scheme
+            defined in Training configuration.
+        """
+        # Create Training Environment
+        self._log_agent("Creating environment:")
+        train_env = self._create_tfagent_env()
+        observation_spec = train_env.observation_spec()
+        action_spec = train_env.action_spec()
+        timestep_spec = train_env.time_step_spec()
+
+        # SetUp Optimizer, Networks and PpoAgent
+        self._log_agent("Creating agent:")
+        self._log_agent("  creating  tf.compat.v1.train.AdamOptimizer( ... )")
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=self._learning_rate)
+
+        actor_net = actor_distribution_network.ActorDistributionNetwork(observation_spec,
+                                                                        action_spec,
+                                                                        fc_layer_params=self.fc_layers)
+
+        self._log_agent("  creating  ReinforceAgent( ... )")
+        tf_agent = reinforce_agent.ReinforceAgent(timestep_spec, action_spec,
+                                                  actor_network=actor_net,
+                                                  optimizer=optimizer)
+        self._log_agent("  executing tf_agent.initialize()")
+        tf_agent.initialize()
+        self._trained_policy = tf_agent.policy
+
+        # SetUp Data collection & Buffering
+        self._log_agent("Creating data collection:")
+        collect_data_spec = tf_agent.collect_data_spec
+        self._log_agent("  creating TFUniformReplayBuffer()")
+        replay_buffer = TFUniformReplayBuffer(collect_data_spec,
+                                              batch_size=1,
+                                              max_length=self._num_training_steps_in_replay_buffer)
+
+        collect_policy = tf_agent.collect_policy
+        self._log_agent("  creating DynamicEpisodeDriver()")
+        collect_driver = DynamicEpisodeDriver(train_env,
+                                              collect_policy,
+                                              observers=[replay_buffer.add_batch],
+                                              num_episodes=self._training.num_episodes_per_iteration)
+
+        # Train
+        collect_driver.run = common.function(collect_driver.run, autograph=False)
+        tf_agent.train = common.function(tf_agent.train, autograph=False)
+
+        self._log_agent("Starting training:")
+        self._train_iteration_completed(0)
+        # OZ->CHH: as there is num_epochs parameter for VPG, should we apply it here? 
+        # Should we have it in the training config in the first place as it seems to be specific to PPO (neither DQN nor VPG seem to have it)? 
+        iterations = self._training.num_epochs_per_iteration * self._training.num_iterations
+        for iteration in range(1, iterations + 1):
+            msg = f'training {iteration:4} of {self._training.num_iterations:<4}:'
+            self._log_agent(msg + " executing collect_driver.run()")
+            collect_driver.run()
+
+            self._log_agent(msg + " executing replay_buffer.gather_all()")
+            trajectories = replay_buffer.gather_all()
+
+            self._log_agent(msg + " executing tf_agent.train(...)")
+            total_loss, _ = tf_agent.train(experience=trajectories)
+
+            self._log_agent(msg + " executing replay_buffer.clear()")
+            replay_buffer.clear()
+
+            self._train_iteration_completed(iteration, total_loss)
+        return
+
+VpgAgent = ReinforceAgent        
