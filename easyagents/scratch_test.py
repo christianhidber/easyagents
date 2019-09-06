824db0c2d99606f0717bc92e09a93dd3db968520
Christian Hidber <github@bsquare.ch>
2019-09-06 15:15:55 +0200

    fix unit tests

--- easyagents/scratch_test.py
+++
@@ -1,8 +0,0 @@
-import easyagents
-from easyagents.callbacks.duration import Fast
-from easyagents.callbacks.plot import PlotLoss, PlotRewards, PlotSteps, PlotState
-
-agent = easyagents.PpoAgent("CartPole-v0")
-agent.train([Fast(), PlotRewards()])
-
-input('press enter')
