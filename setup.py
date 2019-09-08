9881d5522e828b5e7a561cea9a9b701852d690af
Christian Hidber <github@bsquare.ch>
2019-09-08 09:44:44 +0200

    cleanup
    cleanup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="easyagents-v1",
-    version="1.0a2",
+    version="1.0a3",
     description="reinforcement learning for practitioners.",
     long_description=README,
     long_description_content_type="text/markdown",
