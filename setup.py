273e9c1d69e9b195b22f78d64f6682ad0e76fd5f
Christian Hidber <github@bsquare.ch>
2019-09-28 22:09:55 +0200

    tensorforce pytest success

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="easyagents",
-    version="1.1.14",
+    version="1.1.15",
     description="reinforcement learning for practitioners.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -27,7 +27,7 @@ setup(
     install_requires=["gast==0.2.2", "gym==0.10.11", "imageio==2.4.0", "imageio-ffmpeg==0.3.0",
                       "matplotlib==3.1.1", "numpy==1.17.0", "PILLOW==6.1.0", "pyglet==1.3.2",
                       "tensorforce==0.5.1",
-                      "tensorflow_probability", "tensorflow-estimator",
+                      "tensorflow_probability", "tensorflow-estimator==1.14.0",
                       "tf-agents-nightly", "tf-nightly"],
     include_package_data=True,
     entry_points={
