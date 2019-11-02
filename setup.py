e06811d29478ce4473b1f4b979c3d59d17ff5774
Christian Hidber <github@bsquare.ch>
2019-11-02 18:34:47 +0100

    Deployed  with MkDocs version: 1.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="easyagents",
-    version="1.2.2",
+    version="1.3.0",
     description="reinforcement learning for practitioners.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,11 +24,10 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     packages=["easyagents", "easyagents.callbacks", "easyagents.backends"],
-    install_requires=["gast==0.2.2", "gym==0.10.11", "imageio==2.4.0", "imageio-ffmpeg==0.3.0",
-                      "matplotlib==3.1.1", "numpy==1.17.0", "PILLOW==6.1.0", "pyglet==1.3.2",
-                      "Keras==2.3.0", "keras-rl==0.4.2", "tensorforce==0.5.1",
-                      "tensorflow-probability==0.7.0", "tensorflow-estimator==1.14.0",
-                      "tf-agents-nightly==0.2.0.dev20190928", "tf-nightly==1.15.0.dev20190821"],
+    install_requires=[
+        "tf-agents-nightly==0.2.0.dev20191102", "tensorflow==2.0.0", "gym==0.15.3",
+        "imageio==2.6.1", "imageio-ffmpeg==0.3.0", "matplotlib==3.1.1"
+    ],
     include_package_data=True,
     entry_points={
         "console_scripts": [
