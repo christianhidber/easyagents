d3842afd09cd8cbd74ab5feb534d60833c787c1c
Christian Hidber <github@bsquare.ch>
2019-11-25 22:52:06 +0100

    tforce_test

--- easyagents/backends/tforce_test.py
+++ easyagents/backends/tforce_test.py
@@ -17,14 +17,16 @@ class TensorforceAgentTest(unittest.TestCase):
         self.env_name = env._StepCountEnv.register_with_gym()
 
     def test_dqn_train(self):
-        model_config = core.ModelConfig(_cartpole_name, fc_layers=(100,))
+        model_config = core.ModelConfig(_cartpole_name, fc_layers=(100,100))
         tc: core.StepsTrainContext = core.StepsTrainContext()
-        tc.num_iterations = 2000
-        tc.num_steps_buffer_preload = 100
-        tc.num_iterations_between_eval = 100
-        tc.max_steps_per_episode = 20
+        tc.num_iterations = 10000
+        tc.num_steps_buffer_preload = 500
+        tc.num_iterations_between_eval = 500
+        tc.max_steps_per_episode = 200
         dqnAgent = tforce.TforceDqnAgent(model_config=model_config)
         dqnAgent.train(train_context=tc, callbacks=[log.Iteration(eval_only=True), log.Agent()])
+        (min_r,avg_r,max_r) = tc.eval_rewards[tc.episodes_done_in_training]
+        assert avg_r > 100
 
     def test_dueling_dqn_train(self):
         model_config = core.ModelConfig(_cartpole_name, fc_layers=(100,))
@@ -32,27 +34,36 @@ class TensorforceAgentTest(unittest.TestCase):
         tc.num_iterations = 2000
         tc.num_steps_buffer_preload = 100
         tc.num_iterations_between_eval = 100
-        tc.max_steps_per_episode = 20
+        tc.max_steps_per_episode = 200
         dqnAgent = tforce.TforceDuelingDqnAgent(model_config=model_config)
         dqnAgent.train(train_context=tc, callbacks=[log.Iteration(eval_only=True), log.Agent()])
 
     def test_ppo_train(self):
         model_config = core.ModelConfig(_cartpole_name)
         tc = core.PpoTrainContext()
+        tc.num_iterations=20
         ppoAgent = tforce.TforcePpoAgent(model_config=model_config)
-        ppoAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent(), duration.Fast()])
+        ppoAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent()])
+        (min_r,avg_r,max_r) = tc.eval_rewards[tc.episodes_done_in_training]
+        assert avg_r > 100
 
     def test_random_train(self):
         model_config = core.ModelConfig(_cartpole_name)
         tc = core.TrainContext()
+        tc.num_iterations=50
         randomAgent = tforce.TforceRandomAgent(model_config=model_config)
-        randomAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent(), duration.Fast()])
+        randomAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent()])
+        (min_r,avg_r,max_r) = tc.eval_rewards[tc.episodes_done_in_training]
+        assert avg_r < 50
 
     def test_reinforce_train(self):
         model_config = core.ModelConfig(_cartpole_name)
         tc = core.EpisodesTrainContext()
+        tc.num_iterations=50
         reinforceAgent = tforce.TforceReinforceAgent(model_config=model_config)
-        reinforceAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent(), duration.Fast()])
+        reinforceAgent.train(train_context=tc, callbacks=[log.Iteration(), log.Agent()])
+        (min_r,avg_r,max_r) = tc.eval_rewards[tc.episodes_done_in_training]
+        assert avg_r > 100
 
 
 if __name__ == '__main__':
